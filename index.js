0e96415bb6a8ad6e674d73bad1cf9aa192473c83
Vladislav Kaminsky <wlodzislav@ya.ru>
2019-02-01 13:28:00 +0400

    Import code

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 function request2curl(options) {
+	var curl = "curl";
+	if (options.followAllRedirects) {
+		curl += " -v --location";
+	}
+	if (options.method) {
+		curl += " -X " + options.method;
+	}
+	if (options.qs) {
+		curl += " '" + options.url + "?" + querystring.stringify(options.qs) + "'";
+	} else {
+		curl += " '" + options.url + "'";
+	}
+	if (options.form) {
+		curl += " --data '" + querystring.stringify(options.form) + "'";
+	}
+	if (options.body) {
+		curl += " --data '" + options.body + "'";
+	}
+	if (options.auth) {
+		curl += " --user " + options.auth.user + ":" + options.auth.pass;
+	}
+	if (options.proxy) {
+		curl += " --proxy " + options.proxy;
+	}
+	if (options.headers) {
+		for (var headerName in options.headers) {
+			var headersValue = options.headers[headerName];
+			curl += " -H '" + headerName + ":" + headersValue + "'";
+		}
+	}
+	return curl;
 }
 
 module.exports = request2curl;
